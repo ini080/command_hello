08d8603cddf84b0c382686fd89cf2701ab4746c9
ini080 <32252279+ini080@users.noreply.github.com>
2017-09-27 10:07:39 +0900

    added modified

--- test.c
+++ test.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-
 int main()
 {
-<<<<<<< HEAD
 	printf("Hello Git World\n");
 	printf("Tell your Wrold\n");
 	printf("Tell his world\n");
-	pritnf("Tell her world\m");
+	pritnf("Tell her world\n");
+	printf("modified\n");
 	return 0;
 
 }
